456ce709eb10825c9fa2b8c410456e13b00874b5
stoke <stoke95@yahoo.it>
2012-07-01 18:51:39 +0200

    default value

--- test/optimal.js
+++ test/optimal.js
@@ -25,6 +25,11 @@ describe('Optimal', function() {
       existent: {
         type: 'object',
         optional: true
+      },
+
+      nonexistentdefault: {
+        type: 'number',
+        defaultValue: 90
       }
     });
 
@@ -33,15 +38,18 @@ describe('Optimal', function() {
     assert.strictEqual(args.nonexistent, undefined);
     args.fn.should.be.a('function');
     args.existent.should.be.a('object');
+    args.nonexistentdefault.should.be.equal(90);
   });
 
   it('Should parse optional arguments correctly with string', function() {
-    var args = optimal(['test', 1337, function() {}, {'test': 7887}], 's:string, n:[number], o:[nonexistent], f:fn, o:[existent]');
+    var args = optimal(['test', 1337, function() {}, {'test': 7887}], 's:string, n:[number], o:[nonexistent], f:fn, o:[existent], n:[nonexistentdefault=90], s:[stringdefault="test"]');
 
     args.string.should.be.a('string');
     args.number.should.be.a('number');
     assert.strictEqual(args.nonexistent, undefined);
     args.fn.should.be.a('function');
     args.existent.should.be.a('object');
+    args.nonexistentdefault.should.be.equal(90);
+    args.stringdefault.should.be.equal("test");
   });
 });
